a91e8d6f6b0ad185ee5eda3a3729abdc3628176f
cbaronac <41337700+cbaronac@users.noreply.github.com>
2018-10-14 18:28:59 -0500

    Update Node.java

--- laboratorios/lab04/codigo/Node.java
+++ laboratorios/lab04/codigo/Node.java
@@ -10,8 +10,13 @@ public class Node
     public Node left;
     public Node right;
     public int data;
+    public String persona;
     
    public Node(int data){
        this.data=data;
+    }
+    
+    public Node(String persona) {
+        this.persona=persona;
     }
 }
