4cb8d2f16ceb55d48e1cf3ac1bf5e19936838b47
cbaronac <41337700+cbaronac@users.noreply.github.com>
2018-11-05 19:00:57 -0500

    Add files via upload

---
+++ proyecto/codigo/Abeja.java
@@ -0,0 +1,50 @@
+public class Abeja {
+    Double x;
+    Double y;
+    Double z;
+    boolean colision;
+    
+    public Abeja(String x, String y, String z){
+        this.x=Double.parseDouble(x);
+        this.y=Double.parseDouble(y);
+        this.z=Double.parseDouble(z);
+    }
+    
+    public void setColision(boolean colision){
+        this.colision=colision;
+    }
+    
+    public boolean getColision(boolean colision){
+        return colision;
+    }
+    
+    public void setX(Double x){
+    this.x=x;
+    }
+    
+    public void setY(Double y){
+    this.y=y;
+    }
+    
+    public void setZ(Double z){
+    this.z=z;
+    }
+    
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+    return y;
+    }
+    
+    public double getZ(){
+        return z;
+    }
+    
+    public String toString(){
+        String mensaje=x+","+y+","+z;
+        return mensaje;
+    }
+    
+}
